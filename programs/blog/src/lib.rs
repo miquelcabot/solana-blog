ab98e06d4c1377b3ba458e4f7fd32721bf248647
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-08-04 19:25:47 +0200

    add CreatePost struct

--- programs/blog/src/lib.rs
+++ programs/blog/src/lib.rs
@@ -24,6 +24,22 @@ pub struct Initialize<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+#[instruction(title: String, content: String)]
+pub struct CreatePost<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(mut, has_one = authority)]
+    pub blog: Account<'info, Blog>,
+
+    #[account(init, seeds = [b"post", blog.key().as_ref(), &blog.posts.to_be_bytes()], bump, payer = authority, 
+        space = 8 + size_of::<Post>() + title.as_bytes().len() + content.as_bytes().len() - 40)]
+    pub post: Account<'info, Post>,
+
+    pub system_program: Program<'info, System>,
+}
+
 #[account]
 #[derive(Default)]
 pub struct Blog {
